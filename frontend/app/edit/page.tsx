b90c8b5fd5f1b5f358babb7b25236f03e1ecf75e
michael-tyl <mliuty2016@gmail.com>
2024-10-13 12:25:45 -0700

    add outspeed

--- frontend/app/edit/page.tsx
+++ frontend/app/edit/page.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import Document from "./document"
+import { useWebRTC, RealtimeVideo } from "@outspeed/react";
+
 export default function page() {
+  const { 
+    connect,
+    connectionStatus,
+    getRemoteVideoTrack,
+    getLocalVideoTrack
+    } = useWebRTC({
+      config: {
+        // Add your function URL.
+        functionURL: "<my-function-url>", 
+        audio: true,
+        video: false,
+      },
+    });
+  
   return (
-    <div> <Document/> </div>
+    <div> 
+      <span>Connection Status (for testing): {connectionStatus}</span>
+      <Document/> 
+    </div>
   )
 }
